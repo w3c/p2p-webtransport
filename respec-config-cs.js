c87985bfc0ffc3f1fdeb406e71c5d236ab157f49
Bernard Aboba <bernard.aboba@gmail.com>
2018-11-30 17:11:05 -0800

    Add respec-config file for cs.html

---
+++ respec-config-cs.js
@@ -0,0 +1,147 @@
+var respecConfig = {
+  specStatus: "ED",
+  // if there a publicly available Editor's Draft, this is the link
+  edDraftURI: "https://w3c.github.io/webrtc-quic/cs.html",
+  shortName: "webrtc-quic-cs",
+  editors: [
+    { name: "Peter Thatcher", company: "Google", w3cid: "68236" },
+    { name: "Bernard Aboba", company: "Microsoft Corporation", w3cid: "65611" } 
+  ],
+  authors: [
+  ],
+  wg: "Web Real-Time Communications Working Group",
+  wgURI: "https://www.w3.org/2011/04/webrtc/",
+  wgPublicList: "public-webrtc",
+  wgPatentURI:  "https://www.w3.org/2004/01/pp-impl/47318/status",
+  issueBase: "https://github.com/w3c/webrtc-quic/issues",
+  otherLinks: [
+    {
+      key: "Participate",
+      data: [
+        {
+          value: "Mailing list",
+          href: "https://lists.w3.org/Archives/Public/public-webrtc/"
+        },
+        {
+          value: "Browse open issues",
+          href: "https://github.com/w3c/webrtc-quic/issues"
+        },
+        {
+          value: "IETF QUIC Working Group",
+          href: "https://tools.ietf.org/wg/quic/"
+        }
+      ]
+    }
+  ],
+  localBiblio: {
+    "IANA-STUN-6": {
+      "title": "STUN Error Codes",
+      "href": "https://www.iana.org/assignments/stun-parameters/stun-parameters.xhtml#stun-parameters-6",
+      "publisher": "IANA"
+    },
+      "QUIC-DATAGRAM": {
+      "title": "An Unreliable Datagram Extension to QUIC",
+      "href": "https://tools.ietf.org/html/draft-pauly-quic-datagram",
+      "authors": [
+        "T. Pauly",
+        "E. Kinnear",
+        "D. Schinazi"
+      ],
+      "status": "10 September 2018. Internet draft (work in progress)",
+      "publisher": "IETF"
+    },
+    "QUIC-TRANSPORT": {
+      "title": "QUIC: A UDP-Based Multiplexed and Secure Transport",
+      "href": "https://tools.ietf.org/html/draft-ietf-quic-transport",
+      "authors": [
+        "J. Iyengar",
+        "M. Thomson"
+      ],
+      "status": "23 October 2018. Internet draft (work in progress)",
+      "publisher": "IETF"
+    },
+    "QUIC-MULT": {
+      "title": "QUIC Multiplexing",
+      "href": "https://tools.ietf.org/html/draft-aboba-avtcore-quic-multiplexing",
+      "authors": [
+        "B. Aboba",
+        "P. Thatcher",
+        "C. Perkins"
+      ],
+      "status": "23 October 2018. Internet draft (work in progress)",
+      "publisher": "IETF"
+    },
+    "RFC7675": {
+      "title": "Session Traversal Utilities for NAT (STUN) Usage for Consent Freshness",
+      "href": "https://tools.ietf.org/html/rfc7675",
+      "authors": [
+        "M. Perumal",
+        "D. Wing",
+        "R. Ravindranath",
+        "T. Reddy",
+        "M. Thomson"
+      ],
+      "status": "October 2015. RFC",
+      "publisher": "IETF"
+    },
+    "DATA-PROT": {
+      "title": "WebRTC Data Channel Establishment Protocol",
+      "href": "https://tools.ietf.org/html/draft-ietf-rtcweb-data-protocol",
+      "authors": [
+        "R. Jesup",
+        "S. Loreto",
+        "M. Tuexen"
+      ],
+      "status": "04 January 2015. Internet Draft (work in progress)",
+      "publisher": "IETF"
+    },
+    "JSEP": {
+      "title": "Javascript Session Establishment Protocol",
+      "href": "https://tools.ietf.org/html/draft-ietf-rtcweb-jsep",
+      "authors": [
+        "J. Uberti",
+        "C. Jennings",
+        "E. Rescorla"
+      ],
+      "status": "22 October 2018. Internet Draft (work in progress)",
+      "publisher": "IETF"
+    },
+    "RTCWEB-SECURITY": {
+      "title": "Security Considerations for WebRTC",
+      "href": "https://tools.ietf.org/html/draft-ietf-rtcweb-security",
+      "authors": [
+        "E. Rescorla"
+      ],
+      "status": "22 January 2018. Internet Draft (work in progress)",
+      "publisher": "IETF"
+    },
+    "RTCWEB-SECURITY-ARCH": {
+      "title": "WebRTC Security Architecture",
+      "href": "https://tools.ietf.org/html/draft-ietf-rtcweb-security-arch",
+      "authors": [
+        "E. Rescorla"
+      ],
+      "status": "17 November 2018. Internet Draft (work in progress)",
+      "publisher": "IETF"
+    },
+    "TLS13": {
+      "title": "The Transport Layer Security (TLS) Protocol Version 1.3",
+      "href": "https://tools.ietf.org/html/draft-ietf-tls-tls13",
+      "authors": [
+        "E. Rescorla"
+      ],
+      "status": "20 March 2018. Internet Draft (work in progress)",
+      "publisher": "IETF"
+    },
+    "WEBRTC-STATS": {
+      "title": "Identifiers for WebRTC's Statistics API",
+      "href": "https://w3c.github.io/webrtc-stats/",
+      "authors": [
+        "Harald Alvestrand",
+        "Varun Singh"
+      ],
+      "status": "07 November 2018 (work in progress)",
+      "publisher": "W3C"
+    }
+  }
+}

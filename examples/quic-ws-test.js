9fd32ea7b3fea94d8d2176dd6d836f7f1a73a511
Peter Thatcher <pthatcher@chromium.org>
2019-01-03 10:21:52 -0800

    Test a big message.

--- examples/quic-ws-test.js
+++ examples/quic-ws-test.js
@@ -10,20 +10,31 @@ async function testWebSocket(ws) {
   ws.onerror = (evt) => {
     console.log("onerror: " + evt.message);
   };
-
-  console.log("ws.readyState: " + ws.readyState);
-  console.log("ws.binaryType: " + ws.binaryType);
-  const msg1 = new Uint8Array([1, 2, 3, 4]);
   ws.onmessage = (evt) => {
     console.log("Received message: ");
     console.log(evt.data);
   }
+
+  console.log("ws.readyState: " + ws.readyState);
+  console.log("ws.binaryType: " + ws.binaryType);
+
+  const msg1 = new Uint8Array([1, 2, 3, 4]);
   ws.send(msg1);
   console.log("ws.bufferedAmount: " + ws.bufferedAmount);  
-  ws.close(500, "We failed");
 
+  const msg2 = new Uint8Array(repeat([1, 2, 3, 4], 100));
+  ws.send(msg2);
+  console.log("ws.bufferedAmount: " + ws.bufferedAmount);  
+
+  ws.close(500, "We failed");
   console.log("ws.readyState: " + ws.readyState);
 }
 
 testWebSocket(new QuicUnreliableDatagramWebSocket("datagram.us", 12345))
 testWebSocket(new QuicUnreliableStreamWebSocket("stream.us", 54321))
+
+function* repeat(iterable, times) {
+  for (let i = 0; i < times; i++) {
+    yield* iterable;
+  }
+}
